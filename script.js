934c5e2f3d46d68d99b0a8157a3941749ab86a2a
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2023-04-20 20:22:26 -0300

    adicionando JS

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿// Animación de desvanecimiento
+window.onload = function() {
+	var login = document.querySelector(".login");
+	login.style.opacity = 0;
+
+	(function fade() {
+		var val = parseFloat(login.style.opacity);
+		if (!((val += .1) > 1)) {
+			login.style.opacity = val;
+			requestAnimationFrame(fade);
+		}
+	})();
+};
