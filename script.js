8fb9c48f66ad361a7513c7798249d79f07c25f21
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2023-05-01 19:39:54 -0300

    .

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ window.onload = function() {
 		}
 	})();
 };
+
+const form = document.getElementById('cadastro');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const nome = document.getElementById('nome').value;
+  const email = document.getElementById('email').value;
+  const telefone = document.getElementById('telefone').value;
+  const endereco = document.getElementById('endereco').value;
+  console.log(`Nome: ${nome}\nEmail: ${email}\nTelefone: ${telefone}\nEndereço: ${endereco}`);
+  // Aqui você pode adicionar código para enviar os dados do formulário para um servidor ou armazená-los localmente
+});
+
+const menu = document.querySelector('.menu');
+const hamburguer = document.querySelector('.hamburguer');
+
+hamburguer.addEventListener('click', () => {
+  menu.classList.toggle('aberto');
+});
